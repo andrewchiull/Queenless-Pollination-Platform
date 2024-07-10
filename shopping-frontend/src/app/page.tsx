60b7c57628c669d115bc7bd64089934ae835c3cf
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-07-11 04:50:32 +0800

    refactor: Update gallery component and home page layout

--- shopping-frontend/src/app/page.tsx
+++ shopping-frontend/src/app/page.tsx
@@ -1,19 +1,59 @@
 "use client";
 
-import { Button } from '@mui/material';
-import QuiltedImageList from './components/gallery';
-
+import { Button, Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import Gallery from './components/gallery';
+import { styled } from "@mui/material/styles";
+const CardContentCustomPadding = styled(CardContent)(`
+  padding: 0px;
+  &:last-child {
+    padding-bottom: 0px;
+  }
+`);
 
 const Home = () => {
 
   return (
-    <>
-      <QuiltedImageList />
-      https://www.ntu.edu.tw/spotlight/2023/2145_20230329_1.jpg
-      <Button variant="contained" color="warning" onClick={() => window.location.href = '/order'}>
-        立即下單
-      </Button>
-    </>
+    <Container style={{ margin: '2rem 0' }}>
+      <Grid container spacing={1}>
+        <Grid item sx={{ width: '100%' }}>
+          <Card>
+            <CardContentCustomPadding>
+              <Gallery />
+            </CardContentCustomPadding>
+          </Card >
+        </Grid>
+
+        <Grid item sx={{ width: '100%' }}>
+          <Card>
+            <CardContentCustomPadding>
+              <Typography variant="h4"
+                sx={{
+                  height: '10vh', display: 'flex', alignItems: 'center', justifyContent: 'center',
+                  color: 'white', backgroundColor: 'black'
+                }}
+              >
+                無蜂王授粉技術
+              </Typography>
+            </CardContentCustomPadding>
+          </Card >
+        </Grid>
+
+        <Grid item sx={{ width: '100%' }}>
+          <Card>
+            <CardContentCustomPadding>
+              <Button
+                sx={{ width: '100%' }}
+                variant="contained" color="warning" onClick={() => window.location.href = '/order'}>
+                立即下單
+              </Button>
+            </CardContentCustomPadding>
+          </Card >
+        </Grid>
+      </Grid>
+
+
+
+    </Container>
   );
 };
 

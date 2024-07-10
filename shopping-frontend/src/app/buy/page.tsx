cb0d57d902e8b9bef8563de46cdae882b4ca701a
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-07-11 02:15:35 +0800

    refactor: Remove theme to fix dark problems

--- shopping-frontend/src/app/buy/page.tsx
+++ shopping-frontend/src/app/buy/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Grid, ThemeProvider, createTheme } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ProductCard from './components/ProductCard';
@@ -15,21 +15,7 @@ interface Product {
   price: number;
 }
 
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
-
-const Home = () => {
-  useEffect(() => {
-    if (typeof document !== 'undefined') {
-      const metaTag = document.createElement('meta');
-      metaTag.name = 'color-scheme';
-      metaTag.content = 'light dark';
-      document.head.appendChild(metaTag);
-    }
-  }, []);
+const Buy = () => {
 
   const [products, setProducts] = useState<Product[]>([]);
   const [quantities, setQuantities] = useState<{ [key: number]: number }>({});
@@ -50,29 +36,27 @@ const Home = () => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Grid container spacing={4}>
-          {products.map(product => (
-            <Grid item key={product.id} xs={12} sm={6}>
-              <ProductCard
-                product={product}
-                quantity={quantities[product.id]}
-                onQuantityChange={handleQuantityChange}
-              />
-            </Grid>
-          ))}
-        </Grid>
+    <Container style={{ margin: '2rem 0' }}>
+      <Grid container spacing={4}>
+        {products.map(product => (
+          <Grid item key={product.id} xs={12} sm={6}>
+            <ProductCard
+              product={product}
+              quantity={quantities[product.id]}
+              onQuantityChange={handleQuantityChange}
+            />
+          </Grid>
+        ))}
+      </Grid>
 
-        <OrderForm
-          products={products}
-          quantities={quantities}
-          setQuantities={setQuantities}
-        />
-        <ToastContainer />
-      </Container>
-    </ThemeProvider>
+      <OrderForm
+        products={products}
+        quantities={quantities}
+        setQuantities={setQuantities}
+      />
+      <ToastContainer />
+    </Container>
   );
 };
 
-export default Home;
+export default Buy;

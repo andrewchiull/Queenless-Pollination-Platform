6f1cef2b249045d4276b3f16a370c1082f3d27e5
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-07-11 03:54:23 +0800

    Add gallery demo

--- shopping-frontend/src/app/page.tsx
+++ shopping-frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { Button } from '@mui/material';
+import QuiltedImageList from './components/gallery';
 
 
 const Home = () => {
 
   return (
-    <Button variant="contained" color="primary" onClick={() => window.location.href='/order'}>
-      立即下單
-    </Button>
+    <>
+      <QuiltedImageList />
+      https://www.ntu.edu.tw/spotlight/2023/2145_20230329_1.jpg
+      <Button variant="contained" color="warning" onClick={() => window.location.href = '/order'}>
+        立即下單
+      </Button>
+    </>
   );
 };
 

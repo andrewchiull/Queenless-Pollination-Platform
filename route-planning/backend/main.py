8faf2c865990d6a7191b99f84a548c7a4282c424
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-08-03 18:29:37 +0800

    More on Query Parameters

--- route-planning/backend/main.py
+++ route-planning/backend/main.py
@@ -54,3 +54,28 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# [Query Parameters - FastAPI](https://fastapi.tiangolo.com/tutorial/query-params/#multiple-path-and-query-parameters)
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# [Query Parameters - FastAPI](https://fastapi.tiangolo.com/tutorial/query-params/#required-query-parameters)
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item

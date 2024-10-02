e03b9290f9b1710cdab9ae5d6f1ce3d614be96cf
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-10-02 12:00:59 +0800

    DB: chore: Update database configuration to include DB_PORT

--- shopping/backend/src/db.ts
+++ shopping/backend/src/db.ts
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT)
 });
 
 export default pool;

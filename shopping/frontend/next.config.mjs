3c42807c2d41acab31114af4f2a7bc6b462b099a
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-10-02 18:04:34 +0800

    Milestone: Built all shopping functions using `docker-compose`

--- shopping/frontend/next.config.mjs
+++ shopping/frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'http://shopping-backend:5000/api/:path*',
       },
     ];
   },

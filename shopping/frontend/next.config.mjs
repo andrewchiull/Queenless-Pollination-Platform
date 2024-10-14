6c27f7af714aa3485f6499d4c4587192d59513ca
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-10-14 14:29:39 +0800

    Use official NEXT Dockerfile

--- shopping/frontend/next.config.mjs
+++ shopping/frontend/next.config.mjs
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 import dotenv from 'dotenv';
 dotenv.config();
-const apiRewrite = {
-  source: '/api/:path*',
-  destination: process.env.API_URL || 'http://localhost:5000/api/:path*'
-};
+
 const nextConfig = {
   async rewrites() {
     return [
-      apiRewrite,
+      {
+        source: '/api/:path*',
+        destination: process.env.API_URL || 'http://localhost:5000/api/:path*'
+      }
     ];
   },
+  output: "standalone",
 };
-
-console.log('API_URL', process.env.API_URL);
-console.log('Rewrites configuration:', JSON.stringify(apiRewrite, null, 2));
 
 export default nextConfig;

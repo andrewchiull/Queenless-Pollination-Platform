1beee04befbdbd3f21181a4ea476ebe2e7e38aba
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2024-10-15 16:19:18 +0800

    Use SQLModel

--- routing/backend/db.py
+++ routing/backend/db.py
@@ -1,8 +1,9 @@
 import os
+from typing import Annotated
 from dotenv import load_dotenv
 
-from sqlmodel import Session, create_engine
-from sqlalchemy import text
+from fastapi import Depends
+from sqlmodel import Session, create_engine, Field, SQLModel, select
 
 load_dotenv()
 DB_HOST = os.getenv("DB_HOST")
@@ -12,22 +13,45 @@ DB_NAME = os.getenv("DB_NAME")
 
 # mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]
 DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-
 engine = create_engine(DB_URL)
 
 def get_session():
     with Session(engine) as session:
         yield session
 
-def fetch_products():
-    session = get_session()
-    statement = text("SELECT * FROM `queenless-pollination-platform`.products;")
-    results = session.exec(statement)
-    if results:
-        return {"products": results.all()}
-    else:
-        return {"error": "Database not found"}
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+# For SQLModels, use `__tablename__` to specify the actual table name in the database
+# Otherwise, SQLModel will use the class name
+# See https://github.com/fastapi/sqlmodel/issues/159
+class Product(SQLModel, table=True):
+    __tablename__: str = "products"
+
+    id: int = Field(primary_key=True)
+    name: str = Field(index=True)
+    price: float = Field(index=True)
+    description: str = Field(index=True)
+
+class Order(SQLModel, table=True):
+    __tablename__: str = "orders"
+
+    id: int = Field(primary_key=True)
+    name: str = Field(index=True)
+    email: str = Field(index=True)
+    address: str = Field(index=True)
+    product_id: int = Field(index=True)
+
 
 if __name__ == "__main__":
-    print("Testing: fetch_products()")
-    print(fetch_products())
+    print("Testing: read_products()")
+
+    def read_products():
+        with Session(engine) as session:
+            results = session.exec(select(Product))
+            if results:
+                return {"products": results.all()}
+            else:
+                return {"error": "Database not found"}
+
+    print(read_products())
--- routing/backend/main.py
+++ routing/backend/main.py
@@ -1,27 +1,14 @@
 import os
-from enum import Enum
-from typing import Union, List
-from datetime import datetime
-
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
-import httpx
+from sqlmodel import select
 
-from mysql.connector import cursor
-from db import get_db
-from models import Order
+from .db import Order, Product, SessionDep
 
 app = FastAPI()
 
 # API_URL of shopping-backend
 API_URL = os.getenv("API_URL") or "http://localhost:5000/api/"
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
 
 @app.get("/")
 async def read_root():
@@ -30,114 +17,19 @@ async def read_root():
     print("Redirecting to /docs")
     return RedirectResponse(url="/docs")
 
-# Directly access databases
-@app.get("/db")
-async def read_db(db: cursor.MySQLCursor = Depends(get_db)):
+@app.get("/products/")
+def read_products(session: SessionDep) -> list[Product]:
+    products = session.exec(select(Product)).all()
+    return products
 
-    query = "SHOW DATABASES"
-    db.execute(query)
-    result = db.fetchall()
-    if result:
-        return {"databases": result}
-    else:
-        return {"error": "Database not found"}
-    
+@app.get("/orders/")
+def read_orders(session: SessionDep) -> list[Order]:
+    orders = session.exec(select(Order)).all()
+    return orders
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-# [Path Parameters - FastAPI](https://fastapi.tiangolo.com/tutorial/path-params/#predefined-values)
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    print(f"{model_name = }")
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-
-# [Query Parameters - FastAPI](https://fastapi.tiangolo.com/tutorial/query-params/#multiple-path-and-query-parameters)
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# [Query Parameters - FastAPI](https://fastapi.tiangolo.com/tutorial/query-params/#required-query-parameters)
-
-@app.get("/items/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: int | None = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
-
-
-# Access database via shopping-backend
-@app.get("/shopping-backend/products")
-async def get_products_from_shopping_backend():
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(f"{API_URL}/products")
-            response.raise_for_status()
-            return response.json()
-        except httpx.HTTPStatusError as e:
-            raise HTTPException(status_code=e.response.status_code, detail=str(e))
-        except httpx.RequestError as e:
-            raise HTTPException(status_code=500, detail=f"Error communicating with shopping-backend: {str(e)}")
-
-@app.get("/orders/", response_model=List[Order])
-async def get_orders(db: cursor.MySQLCursor = Depends(get_db)):
-    query = "SELECT * FROM orders"
-    
-    try:
-        db.execute(query)
-        orders = db.fetchall()
-        
-        return [
-            Order(
-                id=order[0],
-                name=order[1],
-                email=order[2],
-                address=order[3],
-                product_id=order[4],
-                quantity=order[5],
-                created_at=order[6]
-            )
-            for order in orders
-        ]
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error fetching orders: {str(e)}")
+@app.post("/orders/")
+def create_order(order: Order, session: SessionDep) -> Order:
+    session.add(order)
+    session.commit()
+    session.refresh(order)
+    return order
